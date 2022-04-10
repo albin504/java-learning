731b6678adfe54f13f91ab856025666b599c3887
xushengbin <xushengbin@baidu.com>
2022-04-10 22:05:42 +0800

    基于nio和线程池的socket server

---
+++ socket/src/com/network/KKMultiThreadPoolServer.java
@@ -0,0 +1,34 @@
+package com.network;
+
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class KKMultiServer2 {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 1) {
+            System.err.println("Usage: java KKMultiServer <port number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+        boolean listening = true;
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            while (listening) {
+                System.out.println("pool size" + ((ThreadPoolExecutor)executor).getPoolSize());
+                System.out.println("active count" + ((ThreadPoolExecutor)executor).getActiveCount());
+                executor.submit(new KKMultiServerRunnable(serverSocket.accept()));
+//                new KKMultiServerThread(serverSocket.accept()).start();
+            }
+            executor.shutdown();
+        } catch (IOException e) {
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(-1);
+        }
+    }
+}
